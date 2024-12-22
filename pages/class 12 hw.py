173672f8bed0b45568eb0e0fbc81d2a7f11cab7b
gordon20121224 <gordongg67@gmail.com>
2024-12-22 09:23:04 +0800

    class 12 hw.py

---
+++ pages/class 12 hw.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import os
+
+col_option, col_image = st.columns([1, 2])
+
+with col_option:
+    with st.expander("圖片設定"):
+        image_folder = "image"
+        image_files = os.listdir(image_folder)
+        fruit = st.selectbox("請選擇水果", image_files)
+        size = st.number_input("設定寬度", step=10, value=300, min_value=0)
+
+with col_image:
+    st.image(f"image/{fruit}", width=size)
